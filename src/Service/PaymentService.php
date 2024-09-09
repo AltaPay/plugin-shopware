06cfa89c780ed901cb39ee9a58db2f97f773ed1b
Mahad Ashraf <>
2024-09-09 12:39:39 +0500

    Check ReservedAmount > 0 for successful type payment

--- src/Service/PaymentService.php
+++ src/Service/PaymentService.php
@@ -127,7 +127,7 @@ class PaymentService implements AsynchronousPaymentHandlerInterface
         $status = (string)$result->Body?->Result;
         if ($allRequestParams['type'] == 'paymentAndCapture' and $result->Body->Transactions->Transaction->CapturedAmount > 0) {
             $status = "Success";
-        } elseif ($result->Body->Transactions->Transaction->ReservedAmount > 0) {
+        } elseif ($allRequestParams['type'] == 'payment' and $result->Body->Transactions->Transaction->ReservedAmount > 0) {
             $status = "Success";
         }
         switch ($status) {
